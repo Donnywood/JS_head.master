43fcc16ed74684acf21ca64e026c0037de7fd4f1
Donnywood <blaskwood@gmail.com>
2023-06-20 15:05:33 +0200

    PC start sinch version or MacOS

--- scropt.js
+++ scropt.js
@@ -1,5 +1,77 @@
-"use strict";
-
-if (4 == 9) {
-    console.log('Ok!');
+function sayHello(name) {
+    return 'Привет, ${name}!';
 }
+
+sayHello('Alex');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const hamburger = 5;
+// const fries = 0;
+
+// if (hamburger && fries) {
+//     console.log(' Ready!');
+// }
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 1;
+
+// console.log(hamburger === 3 && cola === 1 && fries)
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && '12312');
+
+// if (hamburger === 3 && cola === 1 && fries) {
+//     console.log('Ready!');
+// } else {
+//     console.log('Idzemy!');
+// }
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+    console.log('Ready!');
+} else {
+    console.log('Idzemy!');
+}
+
+let johnReport, alexReport, samReport, mariaReport ='done';
+console.log (johnReport || alexReport || samReport || mariaReport);
